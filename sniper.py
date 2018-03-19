1d53be82bffadb8db13c5af8eea33494e20c8b92
Henry <hhburnsboy@gmail.com>
2018-03-19 11:18:47 -0400

    made less breakable

--- sniper.py
+++ sniper.py
@@ -2,35 +2,19 @@ import time;
 from selenium import webdriver;
 from bs4 import BeautifulSoup;
 def register(username, password, CRN):
-	login_url = 'https://cas.cc.binghamton.edu/cas/login?service=https%3A%2F%2Fmy.binghamton.edu';
-	#the url to log in to mybinghamton
-	#new_url = 'https://ssb.cc.binghamton.edu/banner/bwskfreg.P_AltPin';
-	#the ur
+	login_url = 'https://ssb.cc.binghamton.edu/banner/twbkwbis.P_GenMenu?name=bmenu.P_MainMnu&msg=WELCOME+Welcome+to+BU+BRAIN+Self+Service'
 	browser = webdriver.Firefox();
 	#makes a firefox browser
 	browser.get(login_url) 
 	#opens the browser to login url
-	username1 = browser.find_element_by_id("username")
-	password1 = browser.find_element_by_id("password")
+	username1 = browser.find_element_by_name("sid")
+	password1 = browser.find_element_by_name("PIN")
 	#select the username and password inputs in the browser
 	username1.send_keys(username)
 	password1.send_keys(password)
 	#fill in the afformentioned inputs with username and password
-	browser.find_element_by_name("submit").click()
+	browser.find_element_by_xpath("//input[@value='Login' and @type='submit']").click()
 	time.sleep(1);
-	#TODO optimize this we can go directly to this website and log
-	#in to avoid ~7 seconds of wait
-	#browser.get('https://ssb.cc.binghamton.edu/banner/twbkwbis.P_GenMenu?name=bmenu.P_MainMnu&msg=WELCOME+Welcome+to+BU+BRAIN+Self+Service');
-	#just got through login screen
-	browser.find_element_by_xpath("/html/body/section/header/div[2]/ul[1]/li/div/a[1]").click()
-	#if this doesn't work go to bu brain and click inspect element on my 
-	#dashboard in google chrome on top of the bu brain icon. from there
-	#right click the highlighted line, click copy Xcode, and put it in here.
-	time.sleep(1)
-	#select bu brain
-	main_window = browser.window_handles[1];
-	browser.switch_to_window(main_window)
-	time.sleep(1)
 	browser.find_element_by_link_text("Student").click()
 	time.sleep(1);
 	#switch windows in bu brain and click on student
@@ -45,9 +29,8 @@ def register(username, password, CRN):
 	for temp in seasons:
 		if lower(temp.text) == lower(season + " " + year):
 			temp.click();
-			break;
 	#click only the semester that matches what was requested
-	browser.find_element_by_xpath('/html/body/div[3]/form/input').click()
+	browser.find_element_by_xpath("//input[@value='Submit' and @type='submit']").click();
 	#submit the semester
 	username1 = browser.find_element_by_id("crn_id1")
 	#put in crn
